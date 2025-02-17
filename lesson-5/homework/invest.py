a0f608f398f7542f95c4f9ea3678f8bde7ec040d
YovkochevPakhlavon <yovqochevp@gmail.com>
2025-02-17 13:18:47 +0500

    only invest function is left

--- lesson-5/homework/invest.py
+++ lesson-5/homework/invest.py
@@ -1,12 +1,3 @@
-def invest(amount, rate, years):
-    i=1
-    while i <= years:
-        value = amount*(1+rate)**i
-        print(f'year {i}: {round(value,2)}')
-        i+=1
-
-invest(100, .05, 4)
-
 def invest():
     amount = float(input('the principal amount: '))
     rate = float(input('rate: '))
